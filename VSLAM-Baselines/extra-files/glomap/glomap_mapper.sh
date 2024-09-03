ef9b29f911fa878e7c5b175b020b48e4d6069522
Fontan <fontanvillacampa@gmail.com>
2024-09-04 09:54:17 +1000

    parse --RelPoseEstimation.max_epipolar_error

--- VSLAM-Baselines/extra-files/glomap/glomap_mapper.sh
+++ VSLAM-Baselines/extra-files/glomap/glomap_mapper.sh
@@ -14,6 +14,7 @@ exp_folder_colmap="${exp_folder}/colmap_${exp_id}"
 # Reading settings from yaml file
 BundleAdjustment_thres_loss_function=$(yq '.BundleAdjustment.thres_loss_function // 1.0' $settings_yaml)
 RelPoseEstimation_max_epipolar_error=$(yq '.RelPoseEstimation.max_epipolar_error // 1.0' $settings_yaml)
+GlobalPositioning_thres_loss_function=$(yq '.GlobalPositioning.thres_loss_function // 0.10000000000000001' $settings_yaml)
 
 echo "    glomap mapper ..."
 database="${exp_folder_colmap}/colmap_database.db"
@@ -24,6 +25,7 @@ pixi run -e colmap glomap mapper \
     --output_path ${exp_folder_colmap} \
     --skip_view_graph_calibration 1 \
     --RelPoseEstimation.max_epipolar_error "${RelPoseEstimation_max_epipolar_error}" \
+    --GlobalPositioning.thres_loss_function "${GlobalPositioning_thres_loss_function}" \
     --BundleAdjustment.optimize_intrinsics 0 \
     --BundleAdjustment.thres_loss_function "${BundleAdjustment_thres_loss_function}"
 
