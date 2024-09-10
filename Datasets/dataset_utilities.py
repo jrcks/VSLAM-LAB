f30e5a068d43f1ba9590963ca87e27f5b6b0aef6
bev-gorry <beverley_g@hotmail.com>
2024-09-10 14:40:05 +1000

    Add caves to utilities.

--- Datasets/dataset_utilities.py
+++ Datasets/dataset_utilities.py
@@ -11,6 +11,7 @@ from Datasets.dataset_tartanair import TARTANAIR_dataset
 from Datasets.dataset_drunkards import DRUNKARDS_dataset
 from Datasets.dataset_replica import REPLICA_dataset
 from Datasets.dataset_hamlyn import HAMLYN_dataset
+from Datasets.dataset_caves import CAVES_dataset
 
 SCRIPT_LABEL = "[dataset_utilities.py] "
 
@@ -30,7 +31,8 @@ def get_dataset(dataset_name, benchmark_path):
         "monotum": lambda: MONOTUM_dataset(benchmark_path),
         "nuim": lambda: NUIM_dataset(benchmark_path),
         "7scenes": lambda: SEVENSCENES_dataset(benchmark_path),
-        "tartanair": lambda: TARTANAIR_dataset(benchmark_path)
+        "tartanair": lambda: TARTANAIR_dataset(benchmark_path),
+        "caves": lambda: CAVES_dataset(benchmark_path),
     }
 
     return switcher.get(dataset_name, lambda: "Invalid case")()
