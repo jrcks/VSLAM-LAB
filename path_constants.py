c1de342f8eb42cf7a898c13069180e8f9dde3692
Fontan <fontanvillacampa@gmail.com>
2024-09-23 14:50:28 +1000

    add debug config

--- path_constants.py
+++ path_constants.py
@@ -9,8 +9,8 @@ VSLAMLAB_EVALUATION = os.path.join(VSLAM_LAB_PATH, 'VSLAM-LAB-Evaluation')
 VSLAMLAB_BASELINES = os.path.join(VSLAM_LAB_DIR, 'Baselines')
 
 COMPARISONS_YAML_DEFAULT = os.path.join(VSLAM_LAB_DIR, 'configs', 'comp_complete.yaml')
-EXP_YAML_DEFAULT = os.path.join(VSLAM_LAB_DIR, 'configs', 'exp_demo.yaml')
-CONFIG_DEFAULT = 'config_demo.yaml'
+EXP_YAML_DEFAULT = os.path.join(VSLAM_LAB_DIR, 'configs', 'exp_debug.yaml')
+CONFIG_DEFAULT = 'config_debug.yaml'
 
 VSLAM_LAB_EVALUATION_FOLDER = 'vslamlab_evaluation'
 RGB_BASE_FOLDER = 'rgb'
