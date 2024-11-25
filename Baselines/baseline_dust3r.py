5ab9e87df0ea844ff06b87469c2ad3aecba14f80
Fontan <fontanvillacampa@gmail.com>
2024-11-26 09:44:40 +1000

    dust3r demo fixed

--- Baselines/baseline_dust3r.py
+++ Baselines/baseline_dust3r.py
@@ -8,10 +8,26 @@ class DUST3R_baseline(BaselineVSLAMLab):
         baseline_name = 'dust3r'
         baseline_folder = 'dust3r'
         baseline_path = os.path.join(VSLAMLAB_BASELINES, baseline_folder)
-        default_parameters = ['-v']
+        default_parameters = {'verbose': 1, 'max_rgb': 10}
 
         # Initialize the baseline
         super().__init__(baseline_name, baselines_path)
         self.label = f"\033[96m{baseline_name}\033[0m"
         self.baseline_path = baseline_path
         self.default_parameters = default_parameters
+        self.settings_yaml = os.path.join(VSLAMLAB_BASELINES, baseline_folder, f'{baseline_name}_settings.yaml')
+
+    def build_execute_command(self, exp_it, exp, dataset, sequence_name):
+        vslamlab_command = super().build_execute_command_python(exp_it, exp, dataset, sequence_name)
+
+        exec_command = []
+        verbose = self.default_parameters['verbose']
+        if 'verbose' in exp.parameters:
+            verbose = exp.parameters['verbose']
+        if verbose:
+            exec_command += [f'--verbose']
+
+        command_str = ' '.join(exec_command)
+        vslamlab_command = vslamlab_command + " " + command_str
+
+        return vslamlab_command
--- Baselines/extra-files/dust3r/dust3r_execute.py
+++ Baselines/extra-files/dust3r/dust3r_execute.py
@@ -1,27 +1,31 @@
-from dust3r.inference import inference
-from dust3r.model import AsymmetricCroCo3DStereo
-from dust3r.utils.image import load_images
-from dust3r.image_pairs import make_pairs
-from dust3r.cloud_opt import global_aligner, GlobalAlignerMode
-
-from downsample_rgb_frames import downsample_rgb_frames
 
 import os
+import sys
 import torch
 import subprocess
 import argparse
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+dust3r_path = os.path.join(os.getcwd(), 'Baselines', 'dust3r')
+sys.path.append(dust3r_path)
+
+from dust3r.inference import inference
+from dust3r.model import AsymmetricCroCo3DStereo
+from dust3r.utils.image import load_images
+from dust3r.image_pairs import make_pairs
+from dust3r.cloud_opt import global_aligner, GlobalAlignerMode
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description=f"{__file__}")
-    parser.add_argument('sequence_path', type=str, help="Path to the sequence directory.")
-    parser.add_argument('exp_folder', type=str, help=f"Path to sequence.")
-    parser.add_argument('exp_id', type=str, help=f"Path to sequence.")
-    parser.add_argument('--max_rgb', type=int, default=10, help="Maximum number of RGB images.")
-    parser.add_argument('--min_fps', type=float, default=10.0, help="Minimum downsampled frames per second.")
-    parser.add_argument('-v', '--verbose', action='store_true', help="Enable verbose output")
+    parser.add_argument("--sequence_path", type=str, help="path to image directory")
+    parser.add_argument("--calibration_yaml", type=str, help="path to calibration file")
+    parser.add_argument("--rgb_txt", type=str, help="path to image list")
+    parser.add_argument("--exp_folder", type=str, help="path to save results")
+    parser.add_argument("--exp_it", type=str, help="experiment iteration")
+    parser.add_argument("--settings_yaml", type=str, help="settings_yaml")
+    parser.add_argument("--verbose", action="store_true")
 
     parser.add_argument('--device', type=str, default='cuda', help="")
     parser.add_argument('--batch_size', type=int, default=1, help="")
@@ -31,10 +35,10 @@ if __name__ == '__main__':
     parser.add_argument('--img_size', type=int, default=512, help="")
 
     args = parser.parse_args()
-    sequence_path = args.sequence_path.replace("sequence_path:", "")
-    rgb_txt = os.path.join(sequence_path, 'rgb.txt')
-    exp_folder = args.exp_folder.replace('exp_folder:', '')
-    exp_id = args.exp_id.replace('exp_id:', '')
+    sequence_path = args.sequence_path
+    rgb_txt = args.rgb_txt
+    exp_folder = args.exp_folder
+    exp_id = args.exp_it
 
     device = args.device
     batch_size = args.batch_size
@@ -55,36 +59,22 @@ if __name__ == '__main__':
     print(f"    CUDA version: {torch.version.cuda}")
     print(f"    Device capability: {torch.cuda.get_device_capability(0)}")
 
-    downsampled_paths, downsampled_timestamps = downsample_rgb_frames(rgb_txt, args.max_rgb, args.min_fps)
-    for i_path, downsampled_path in enumerate(downsampled_paths):
-        downsampled_paths[i_path] = os.path.join(sequence_path, downsampled_path)
-
+    image_list = []
+    timestamps = []
+    with open(rgb_txt, 'r') as file:
+        for line in file:
+            timestamp, path, *extra = line.strip().split(' ')
+            image_list.append(os.path.join(sequence_path, path))
+            timestamps.append(timestamp)
 
     model_name = "naver/DUSt3R_ViTLarge_BaseDecoder_512_dpt"
+
     # you can put the path to a local checkpoint in model_name if needed
     model = AsymmetricCroCo3DStereo.from_pretrained(model_name).to(device)
-    images = load_images(downsampled_paths, size=img_size)
+    images = load_images(image_list, size=img_size)
     pairs = make_pairs(images, scene_graph='complete', prefilter=None, symmetrize=True)
     output = inference(pairs, model, device, batch_size=batch_size)
 
-    # at this stage, you have the raw dust3r predictions
-    #view1, pred1 = output['view1'], output['pred1']
-    #view2, pred2 = output['view2'], output['pred2']
-    # here, view1, pred1, view2, pred2 are dicts of lists of len(2)
-    #  -> because we symmetrize we have (im1, im2) and (im2, im1) pairs
-    # in each view you have:
-    # an integer image identifier: view1['idx'] and view2['idx']
-    # the img: view1['img'] and view2['img']
-    # the image shape: view1['true_shape'] and view2['true_shape']
-    # an instance string output by the dataloader: view1['instance'] and view2['instance']
-    # pred1 and pred2 contains the confidence values: pred1['conf'] and pred2['conf']
-    # pred1 contains 3D points for view1['img'] in view1['img'] space: pred1['pts3d']
-    # pred2 contains 3D points for view2['img'] in view1['img'] space: pred2['pts3d_in_other_view']
-
-    # next we'll use the global_aligner to align the predictions
-    # depending on your task, you may be fine with the raw output and not need it
-    # with only two input images, you could use GlobalAlignerMode.PairViewer: it would just convert the output
-    # if using GlobalAlignerMode.PairViewer, no need to run compute_global_alignment
     scene = global_aligner(output, device=device, mode=GlobalAlignerMode.PointCloudOptimizer)
     loss = scene.compute_global_alignment(init="mst", niter=niter, schedule=schedule, lr=lr)
 
@@ -99,11 +89,10 @@ if __name__ == '__main__':
             rotation = R.from_matrix(rotation_matrix)
             quaternion = rotation.as_quat()
             qx, qy, qz, qw = quaternion[0], quaternion[1], quaternion[2], quaternion[3]
-            ts = downsampled_timestamps[i_pose]
+            ts = timestamps[i_pose]
             line = str(ts) + " " + str(tx) + " " + str(ty) + " " + str(tz) + " " + str(qx) + " " + str(
                 qy) + " " + str(qz) + " " + str(qw) + "\n"
             file.write(line)
 
-    # visualize reconstruction
     if args.verbose:
         scene.show()
