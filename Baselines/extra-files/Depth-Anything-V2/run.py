ef0ff34781cda03047178da7a991952956a7a50c
Fontan <fontanvillacampa@gmail.com>
2024-12-03 11:06:26 +1000

    depth_anything

--- Baselines/extra-files/Depth-Anything-V2/run.py
+++ Baselines/extra-files/Depth-Anything-V2/run.py
@@ -1,71 +1,58 @@
-import argparse
-import cv2
+from sklearn.linear_model import TheilSenRegressor
+from tqdm import tqdm
 import numpy as np
-import os
+import argparse
+import shutil
 import torch
 import json
-from tqdm import tqdm
 import sys
-import shutil
+import cv2
+import os
 
 Depth_Anything_V2_path = os.path.join(os.getcwd(), 'Baselines', 'Depth-Anything-V2')
 checkpoints_path = os.path.join(Depth_Anything_V2_path, 'checkpoints')
 sys.path.append(Depth_Anything_V2_path)
-
 from depth_anything_v2.dpt import DepthAnythingV2
-
-def analyze_image(raw_image):
-    if raw_image is None:
-        raise FileNotFoundError(f"Image file '{filename}' not found.")
-
-    # Flatten the image to 1D for statistical analysis
-    flat_image = raw_image.flatten()
-
-    # Calculate statistics
-    mean_val = np.mean(flat_image)
-    median_val = np.median(flat_image)
-    max_val = np.max(flat_image)
-    min_val = np.min(flat_image)
-
-    # Find the minimum value different from zero
-    non_zero_pixels = flat_image[flat_image > 0]  # Exclude zero values
-    min_non_zero_val = np.min(non_zero_pixels) if non_zero_pixels.size > 0 else 0
-
-    stats = {
-        "mean": mean_val,
-        "median": median_val,
-        "max": max_val,
-        "min": min_val,
-        "min_non_zero": min_non_zero_val,
-    }
-    print("Image Statistics:")
-    for key, value in stats.items():
-        print(f"{key}: {value}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Depth Anything V2')
 
     parser.add_argument('--sequence_path', type=str)
-    parser.add_argument('--rgb_txt', type=str)
 
+    parser.add_argument('--rgb_txt', type=str, default="none", help="Specify the RGB text (default: 'none')")
+    parser.add_argument('--anchor', action='store_true', default=False, help="Scale predicted depth with information from a sensor")
     parser.add_argument('--input-size', type=int, default=518)
     parser.add_argument('--encoder', type=str, default='vitl', choices=['vits', 'vitb', 'vitl', 'vitg'])
-    parser.add_argument('--max-depth', type=float, default=13)
+    parser.add_argument('--max-depth', type=float, default=8.0)
 
     args = parser.parse_args()
     sequence_path = args.sequence_path
-    rgb_txt = args.rgb_txt
 
-    # Check depth folder
-    depth_folder = os.path.join(sequence_path, 'depth_anything_v2')
-    #if os.path.exists(depth_folder):
-    #    shutil.rmtree(depth_folder)
+    if args.rgb_txt == 'none':
+        rgb_txt = os.path.join(sequence_path, 'rgb.txt')
+    else:
+        rgb_txt = args.rgb_txt
+
+    # Constants
+    depth_folder_name = 'depth_anything_v2'
+    subsample_size = 1000
+    max_depth = args.max_depth
+    min_depth = 0.5
+    scale_factor = 6553.5
+
+    # Prepare depth folder
+    depth_folder = os.path.join(sequence_path, depth_folder_name)
+    depth_folder_anchor = os.path.join(sequence_path, 'depth')
+
+    if os.path.exists(depth_folder):
+        shutil.rmtree(depth_folder)
     os.makedirs(depth_folder, exist_ok=True)
 
-    rgbd_txt = os.path.join(sequence_path, 'rgbd_depth_anything_v2.txt')
+    rgbd_txt = os.path.join(sequence_path, f'rgbd_{depth_folder_name}.txt')
     if os.path.isfile(rgbd_txt):
         os.remove(rgbd_txt)
 
+    # Create model
     DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 
     model_configs = {
@@ -91,20 +78,55 @@ if __name__ == '__main__':
 
     rgbd_assoc = []
     for k, filename in enumerate(tqdm(rgb_paths)):
-
-        # raw_image = cv2.imread(filename)
-        # depth = depth_anything.infer_image(raw_image, args.input_size)
-        #
-        # zero_values = depth < 0.00000001
-        # depth[zero_values] = -1.0
-        # depth = (1.0 / depth)
-        # depth[zero_values] = 0.0
-        #
-        rgbImage = filename
-        depthImage = os.path.join(depth_folder, os.path.splitext(os.path.basename(filename))[0] + '.png')
-        # depthImage = os.path.join(depth_folder, os.path.splitext(os.path.basename(filename))[0] + '.npy')
+        # Paths
+        rgbImage_path = filename
+        scaledDepthImage_path = os.path.join(depth_folder, os.path.splitext(os.path.basename(filename))[0] + '.png')
+        depthImageAnchor_path = os.path.join(depth_folder_anchor, os.path.splitext(os.path.basename(filename))[0] + '.png')
         # jsonFile = os.path.join(depth_folder, os.path.splitext(os.path.basename(filename))[0] + '.json')
-        #
+
+        # Predict disparities
+        raw_image = cv2.imread(filename)
+        invDepthImage = depth_anything.infer_image(raw_image, args.input_size)
+
+        # Anchoring
+        depthImageAnchor = cv2.imread(depthImageAnchor_path, cv2.IMREAD_UNCHANGED) / 6553.5
+
+        # Filter invalid values
+        valid_mask = (depthImageAnchor > 0) & (invDepthImage > 0)
+        depthImageAnchor_flat = depthImageAnchor[valid_mask].reshape(-1, 1)
+        invDepthImage_flat = invDepthImage[valid_mask].reshape(-1, 1)
+
+        # Subsampling for efficiency
+        if len(depthImageAnchor_flat) > subsample_size:
+            indices = np.random.choice(len(depthImageAnchor_flat), subsample_size, replace=False)
+            depthImageAnchor_flat = depthImageAnchor_flat[indices]
+            invDepthImage_flat = invDepthImage_flat[indices]
+
+        # Invert anchor depth
+        invDepthImageAnchor_flat = 1.0 / depthImageAnchor_flat
+
+        # Estimate regression between inverse depths
+        regressor = TheilSenRegressor(random_state=42)
+        regressor.fit(invDepthImage_flat, invDepthImageAnchor_flat.ravel())
+        scaledInvDepthImage = regressor.predict(invDepthImage.flatten().reshape(-1, 1)).reshape(invDepthImage.shape)
+        print(f"Regression Coefficients:")
+        print(f"  Slope: {regressor.coef_[0]:.4f}")
+        print(f"  Intercept: {regressor.intercept_:.4f}")
+
+        # Save predicted depth
+        scaledDepthImage = np.divide(1.0, scaledInvDepthImage, where=scaledInvDepthImage != 0.0)
+        scaledDepthImage = np.where((scaledDepthImage < min_depth) | (scaledDepthImage > max_depth), 0.0, scaledDepthImage)
+
+        #scaledInvDepthImage[scaledInvDepthImage < 0.0000001] = -1.0
+        #scaledDepthImage = 1.0 / scaledInvDepthImage
+        #scaledDepthImage[scaledDepthImage < min_depth] = 0.0
+        #scaledDepthImage[scaledDepthImage > max_depth] = 0.0
+
+        scaledDepthImage = scaledDepthImage * scale_factor
+        scaledDepthImage = scaledDepthImage.astype(np.uint16)
+        cv2.imwrite(scaledDepthImage_path, scaledDepthImage)
+
+        # Save metadata
         # metadata = {
         #     "depthMin": str(depth.min()),
         #     "depthMax": str(depth.max()),
@@ -115,15 +137,14 @@ if __name__ == '__main__':
         #     "type": str("uint16"),
         #     "Description": "Normalized Depth image"
         # }
-        #
-        # np.save(depthImage, depth)
 
         # with open(jsonFile, 'w') as json_file:
         #     json.dump(metadata, json_file)
 
-        rgbd_assoc.append(f"{rgb_timestamps[k]} rgb/{os.path.basename(rgbImage)}  "
-                          f"{rgb_timestamps[k]} depth_anything_v2/{os.path.basename(depthImage)}")
+        rgbd_assoc.append(f"{rgb_timestamps[k]} rgb/{os.path.basename(rgbImage_path)}  "
+                          f"{rgb_timestamps[k]} {depth_folder_name}/{os.path.basename(scaledDepthImage_path)}")
 
+    # Save associations file
     with open(rgbd_txt, 'w') as file:
         for line in rgbd_assoc:
             file.write(line + '\n')
