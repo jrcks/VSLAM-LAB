12591b1810a962e6cea7c62f8095db1c012e545f
Fontan <fontanvillacampa@gmail.com>
2024-12-12 11:55:07 +1000

    yandiwanba dataset

--- Datasets/dataset_utilities.py
+++ Datasets/dataset_utilities.py
@@ -15,6 +15,7 @@ from Datasets.dataset_hamlyn import HAMLYN_dataset
 from Datasets.dataset_caves import CAVES_dataset
 from Datasets.dataset_lamar import LAMAR_dataset
 from Datasets.dataset_eth3d_mvs_dslr import ETH3D_MVS_DSLR_dataset
+from Datasets.dataset_yandiwanba import YANDIWANBA_dataset
 
 SCRIPT_LABEL = "[dataset_utilities.py] "
 
@@ -39,6 +40,7 @@ def get_dataset(dataset_name, benchmark_path):
         "caves": lambda: CAVES_dataset(benchmark_path),
         "lamar": lambda: LAMAR_dataset(benchmark_path),
         "eth3d_mvs_dslr": lambda: ETH3D_MVS_DSLR_dataset(benchmark_path),
+        "yandiwanba": lambda: YANDIWANBA_dataset(benchmark_path)
     }
 
     return switcher.get(dataset_name, lambda: "Invalid case")()
