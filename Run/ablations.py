df6402afc102c1a769d09b5d230c7e276886fcd1
alejandrofontan <fontanvillacampa@gmail.com>
2025-01-24 17:38:27 +1000

    image noise ablation

--- Run/ablations.py
+++ Run/ablations.py
@@ -12,6 +12,7 @@ from path_constants import ABLATION_PARAMETERS_CSV
 
 from utilities import ws
 from Baselines.baseline_utilities import append_ablation_parameters_to_csv
+from Datasets.dataset_utilities import load_rgb_txt
 
 SCRIPT_LABEL = f"\033[35m[{os.path.basename(__file__)}]\033[0m "
 
@@ -37,6 +38,8 @@ def prepare_ablation(exp_it, exp, baseline, dataset, sequence_name, exec_command
         if parameter == 'exp_it':
             continue
         if parameter == 'image_noise':
+            print(f"{ws(8)}image_noise: std: {value}")
+            add_noise_to_images_start(exp_it, exp, dataset, sequence_name, value)
             continue
         section_name, parameter_name = parameter.split('.', 1)
         baseline.modify_yaml_parameter(settings_ablation_yaml, section_name, parameter_name, value)
@@ -52,35 +55,21 @@ def finish_ablation(exp_it, baseline, dataset, sequence_name):
 
 
 
-def add_noise_to_images_start(exp_it, exp, dataset, sequence_name):
+def add_noise_to_images_start(exp_it, exp, dataset, sequence_name, image_noise):
     sequence_path = os.path.join(dataset.dataset_path, sequence_name)
     exp_folder = os.path.join(exp.folder, dataset.dataset_folder, sequence_name)
     rgb_exp_txt = os.path.join(exp_folder, f"{RGB_BASE_FOLDER}_exp.txt")
 
-    # update rgb.txt
-    def std_noise_ablation(it_):
-        if it_ < 500:
-            return 0.0
-        else:
-            return exp.parameters['image_noise']
-
-    std_noise = std_noise_ablation(exp_it)
-    ablation_parameters = {"std_noise": std_noise}
-
-    source_code = inspect.getsource(std_noise_ablation)
-    noise_policy = source_code[source_code.find('return') + len('return'):].strip()
-    print(f"{SCRIPT_LABEL} Noise policy: std_noise = {noise_policy}")
-    print(f"    exp_it = {exp_it}")
-    print(f"    std_noise = {std_noise}")
-
+    std_noise = image_noise
+    
     if std_noise == 0.0:
-        return ablation_parameters
+        return 
 
     # Create rgb_path_ablation folder to store new images
     rgb_path_ablation = os.path.join(sequence_path, f"{RGB_BASE_FOLDER}_ablation")
     if os.path.exists(rgb_path_ablation):
         shutil.rmtree(rgb_path_ablation)
-        #return ablation_parameters
+        #return
     os.makedirs(rgb_path_ablation, exist_ok=True)
 
     def add_gaussian_noise(image_, mean=0, std_dev=25):
@@ -90,25 +79,23 @@ def add_noise_to_images_start(exp_it, exp, dataset, sequence_name):
         noisy_image_ = np.clip(noisy_image_, 0, 255).astype(np.uint8)
         return noisy_image_
 
-    rgb_paths = []
-    rgb_timestamps = []
     with open(rgb_exp_txt, 'r') as file:
-        for line in file:
-            timestamp, path = line.strip().split(' ')
-            rgb_paths.append(path)
-            rgb_timestamps.append(float(timestamp))
+        lines = file.readlines()
 
+    rgb_paths, *_ = load_rgb_txt(rgb_exp_txt)
     with open(rgb_exp_txt, 'w') as file:
-        for i, rgb_path in enumerate(rgb_paths):
+        for i, line in enumerate(lines):
+            rgb_path = rgb_paths[i]
+            file.write(line.replace(f"{RGB_BASE_FOLDER}/", f"{RGB_BASE_FOLDER}_ablation/"))
+
             rgb_file = os.path.join(sequence_path, rgb_path)
-            rgb_ablation_path = rgb_path.replace(f"{RGB_BASE_FOLDER}/", f"{RGB_BASE_FOLDER}_ablation/")
-            rgb_file_ablation = os.path.join(sequence_path, rgb_ablation_path)
             image = cv2.imread(rgb_file)
             noisy_image = add_gaussian_noise(image, mean=0, std_dev=std_noise)
-            cv2.imwrite(os.path.join(sequence_path, rgb_file_ablation), noisy_image)
-            file.write(f"{rgb_timestamps[i]} {rgb_ablation_path}\n")
 
-    return ablation_parameters
+            rgb_ablation_path = rgb_path.replace(f"{RGB_BASE_FOLDER}/", f"{RGB_BASE_FOLDER}_ablation/")
+            rgb_file_ablation = os.path.join(sequence_path, rgb_ablation_path)
+            cv2.imwrite(os.path.join(sequence_path, rgb_file_ablation), noisy_image)
+    return
 
 
 def add_noise_to_images_finish(sequence_path, exp_it):
