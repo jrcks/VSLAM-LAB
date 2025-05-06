a518487614e0cfd17d5bd0a4f72171c86a87ea2a
alejandrofontan <fontanvillacampa@gmail.com>
2025-05-06 15:50:17 +1000

    clean cloning

--- git-clone.sh
+++ git-clone.sh
@@ -3,30 +3,18 @@
 get_git_url() {
   local git_profile=$1
   local git_repo=$2
-  local ssh=$3
-
-  if [ "$ssh" = true ]; then
-    echo "git@github.com:$git_profile/$git_repo.git"
-  else
-    echo "https://github.com/$git_profile/$git_repo.git"
-  fi
+  echo "https://github.com/$git_profile/$git_repo.git"
 }
 
 git_profile=$1
 git_repo=$2
-vslamlab_baselines_folder="$3/$git_repo"
-ssh_flag=$4
+folder_name=${3:-$git_repo}
+vslamlab_baselines_folder="Baselines/${folder_name}"
 
 if [ -d "$vslamlab_baselines_folder" ]; then
   exit 0
 fi
 
-if [ "$ssh_flag" = "--ssh" ]; then
-  ssh=true
-else
-  ssh=false
-fi
+git_url=$(get_git_url "${git_profile}" "${git_repo}")
+git clone --recursive "${git_url}" "${vslamlab_baselines_folder}"
 
-git_url=$(get_git_url ${git_profile} ${git_repo} ${ssh})
-#git clone --recurse-submodules ${git_url} ${vslamlab_baselines_folder}
-git clone --recursive ${git_url} ${vslamlab_baselines_folder}
