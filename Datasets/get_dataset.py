9b7ffd408fab2cf1bfa908c42d0203cb84d0b340
alejandrofontan <fontanvillacampa@gmail.com>
2025-05-11 15:41:55 +1000

    add dataset sweetcorals

--- Datasets/get_dataset.py
+++ Datasets/get_dataset.py
@@ -25,6 +25,7 @@ from Datasets.dataset_squidle import SQUIDLE_dataset
 from Datasets.dataset_openloris import OPENLORIS_dataset
 from Datasets.dataset_madmax import MADMAX_dataset
 from Datasets.dataset_videos import VIDEOS_dataset
+from Datasets.dataset_sweetcorals import SWEETCORALS_dataset
 
 SCRIPT_LABEL = "[dataset_utilities.py] "
 
@@ -58,6 +59,7 @@ def get_dataset(dataset_name, benchmark_path):
         "openloris": lambda: OPENLORIS_dataset(benchmark_path),
         "madmax": lambda: MADMAX_dataset(benchmark_path),
         "videos": lambda: VIDEOS_dataset(benchmark_path),
+        "sweetcorals": lambda: SWEETCORALS_dataset(benchmark_path),
     }
 
     return switcher.get(dataset_name, lambda: "Invalid case")()
