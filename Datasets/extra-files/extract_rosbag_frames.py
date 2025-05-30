a7100ebc8cd6244c958ab52ac51e061d3519c153
alejandrofontan <fontanvillacampa@gmail.com>
2025-05-31 07:41:18 +1000

    ntnu dataset

--- Datasets/extra-files/extract_rosbag_frames.py
+++ Datasets/extra-files/extract_rosbag_frames.py
@@ -9,29 +9,33 @@ import numpy as np
 def main():
     parser = argparse.ArgumentParser(description=f"{__file__}")
 
-    parser.add_argument('rosbag_path', type=str, help=f"rosbag path")
-    parser.add_argument('output_folder', type=str, help=f"output folder")
-    parser.add_argument('image_topic', type=str, help=f"image topic")
-
+    parser.add_argument('--rosbag_path', type=str, help=f"rosbag path")
+    parser.add_argument('--sequence_path', type=str, help=f"sequence_path")
+    parser.add_argument('--image_topic', type=str, help=f"image topic")
 
     args = parser.parse_args()
 
     bridge = CvBridge()
     rosbag_path = args.rosbag_path
     image_topic = args.image_topic
-    output_folder = args.output_folder
-    with rosbag.Bag(rosbag_path, 'r') as bag:
-        for topic, msg, t in tqdm(bag.read_messages(topics=[image_topic])):
-            try:
-                cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-            except Exception as e:
-                print(f"Could not convert image: {e}")
-                continue
+    sequence_path = args.sequence_path
+    rgb_path = os.path.join(sequence_path, 'rgb')
+    rgb_txt = os.path.join(sequence_path, 'rgb.txt')
 
-            image_name = f"{t}.png"
-            image_path = os.path.join(output_folder,image_name)
+    with rosbag.Bag(rosbag_path, 'r') as bag:
+        with open(rgb_txt, 'w') as file:
+            for topic, msg, t in tqdm(bag.read_messages(topics=[image_topic]), desc=f'Extracting frames from {image_topic} ...'):
+                try:
+                    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+                except Exception as e:
+                    print(f"Could not convert image: {e}")
+                    continue
+
+                image_name = f"{t}.png"
+                image_path = os.path.join(rgb_path,image_name)
          
-            cv2.imwrite(image_path, cv_image)
+                cv2.imwrite(image_path, cv_image)
+                file.write(f"{(float(str(t))*10e-10):.5f} rgb/{image_name}\n")
 
 if __name__ == "__main__":
     main()
--- Datasets/get_dataset.py
+++ Datasets/get_dataset.py
@@ -26,6 +26,7 @@ from Datasets.dataset_openloris import OPENLORIS_dataset
 from Datasets.dataset_madmax import MADMAX_dataset
 from Datasets.dataset_videos import VIDEOS_dataset
 from Datasets.dataset_sweetcorals import SWEETCORALS_dataset
+from Datasets.dataset_ntnu_arl_uw import NTNU_ARL_UW_dataset
 
 SCRIPT_LABEL = "[dataset_utilities.py] "
 
@@ -60,6 +61,7 @@ def get_dataset(dataset_name, benchmark_path):
         "madmax": lambda: MADMAX_dataset(benchmark_path),
         "videos": lambda: VIDEOS_dataset(benchmark_path),
         "sweetcorals": lambda: SWEETCORALS_dataset(benchmark_path),
+        "ntnu_arl_uw": lambda: NTNU_ARL_UW_dataset(benchmark_path),
     }
 
     return switcher.get(dataset_name, lambda: "Invalid case")()
