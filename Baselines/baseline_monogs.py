9cde70d1b50ec4abdf5315f1c1268f82679e189f
alejandrofontan <fontanvillacampa@gmail.com>
2025-06-12 18:23:44 +1000

    integrate labels in monogs

--- Baselines/baseline_monogs.py
+++ Baselines/baseline_monogs.py
@@ -8,42 +8,23 @@ SCRIPT_LABEL = f"\033[95m[{os.path.basename(__file__)}]\033[0m "
 class MONOGS_baseline(BaselineVSLAMLab):
     def __init__(self, baseline_name='monogs', baseline_folder='MonoGS'):
 
-        default_parameters = {'verbose': 1}    
+        default_parameters = {'verbose': 1, 'mode': 'mono'}    
         
         # Initialize the baseline
         super().__init__(baseline_name, baseline_folder, default_parameters)
         self.color = 'black'
+        self.modes = ['mono']       
 
     def build_execute_command(self, exp_it, exp, dataset, sequence_name):
-        vslamlab_command = super().build_execute_command_python(exp_it, exp, dataset, sequence_name)
-        return vslamlab_command
-
-    def is_cloned(self):
-        return True
-    
-    def is_installed(self):
-        return True
-    
-    def info_print(self):
-        super().info_print()
-        print(f"Default executable: slam.py")
-
-    def execute(self, command, exp_it, exp_folder, timeout_seconds=1*60*10):
-        self.download_vslamlab_settings()
-        return super().execute(command, exp_it, exp_folder, timeout_seconds)
+        return super().build_execute_command_python(exp_it, exp, dataset, sequence_name)
+        
+    def is_installed(self): 
+        return (True, 'is installed') if self.is_cloned() else (False, 'not installed (conda package available)')
         
 class MONOGS_baseline_dev(MONOGS_baseline):
     def __init__(self):
         super().__init__(baseline_name = 'monogs-dev', baseline_folder =  'MonoGS-DEV')
-        self.color = 'black'
-
-    def is_cloned(self):
-        return os.path.isdir(os.path.join(self.baseline_path, '.git'))
 
     def is_installed(self):
-        return (os.path.isdir(os.path.join(self.baseline_path, 'submodules', 'simple-knn', 'build')) and
-                os.path.isdir(os.path.join(self.baseline_path, 'submodules', 'diff-gaussian-rasterization', 'build')))
-        
-    def info_print(self):
-        super().info_print()
-        print(f"Default executable: {self.baseline_path}/vslamlab_{self.baseline_name}_mono.py")
+        is_installed = os.path.isfile(os.path.join(self.baseline_path, 'build', 'lib.linux-x86_64-cpython-311', 'droid_backends.so'))
+        return (True, 'is installed') if is_installed else (False, 'not installed (auto install available)')
