4e88853ebd5738e5cfcc9f0b3c736417f07851a0
alejandrofontan <fontanvillacampa@gmail.com>
2025-06-12 17:53:29 +1000

    mast3rslam minor fixes

--- Baselines/baseline_mast3rslam.py
+++ Baselines/baseline_mast3rslam.py
@@ -22,21 +22,13 @@ class MAST3RSLAM_baseline(BaselineVSLAMLab):
     def build_execute_command(self, exp_it, exp, dataset, sequence_name):
         return super().build_execute_command_python(exp_it, exp, dataset, sequence_name)        
 
-    def is_installed(self): 
-        return (True, 'is installed') if self.is_cloned() else (False, 'not installed (conda package available)')
-   
-class MAST3RSLAM_baseline_dev(MAST3RSLAM_baseline):
-    def __init__(self):
-        super().__init__(baseline_name = 'mast3rslam-dev', baseline_folder =  'MASt3R-SLAM-DEV')
-
     def git_clone(self):
         super().git_clone()
         self.mast3rslam_download_weights()
 
-    def is_installed(self):
-        is_installed = os.path.isfile(os.path.join(self.baseline_path, f'install_{self.baseline_name}.txt'))
-        return (True, 'is installed') if is_installed else (False, 'not installed (auto install available)')
-
+    def is_installed(self): 
+        return (True, 'is installed') if self.is_cloned() else (False, 'not installed (conda package available)')
+   
     def mast3rslam_download_weights(self): # Download checkpoints
         checkpoints_dir = os.path.join(self.baseline_path,'checkpoints')
         os.makedirs(checkpoints_dir, exist_ok=True)
@@ -51,4 +43,12 @@ class MAST3RSLAM_baseline_dev(MAST3RSLAM_baseline):
             file_name = os.path.basename(file)
             if not os.path.exists(file):
                 print_msg(f"\n{SCRIPT_LABEL}", f"Download weights: {file}",'info')
-                _ = hf_hub_download(repo_id='vslamlab/mast3rslam', filename=file_name, repo_type='model', local_dir=checkpoints_dir)  
+                _ = hf_hub_download(repo_id='vslamlab/mast3rslam_weights', filename=file_name, repo_type='model', local_dir=checkpoints_dir) 
+
+class MAST3RSLAM_baseline_dev(MAST3RSLAM_baseline):
+    def __init__(self):
+        super().__init__(baseline_name = 'mast3rslam-dev', baseline_folder =  'MASt3R-SLAM-DEV')
+
+    def is_installed(self):
+        is_installed = os.path.isfile(os.path.join(self.baseline_path, 'mast3r_slam_backends.so'))
+        return (True, 'is installed') if is_installed else (False, 'not installed (auto install available)')
