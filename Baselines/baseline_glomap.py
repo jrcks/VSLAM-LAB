312abaeaa4695034f5c426492cf6535eb9a8a2b5
alejandrofontan <fontanvillacampa@gmail.com>
2025-06-12 18:59:08 +1000

    add labels to glomap

--- Baselines/baseline_glomap.py
+++ Baselines/baseline_glomap.py
@@ -1,6 +1,5 @@
 import os.path
 
-from utilities import print_msg
 from Baselines.BaselineVSLAMLab import BaselineVSLAMLab
 from Baselines.baseline_colmap import COLMAP_baseline
 
@@ -9,17 +8,17 @@ SCRIPT_LABEL = f"\033[95m[{os.path.basename(__file__)}]\033[0m "
 class GLOMAP_baseline(BaselineVSLAMLab):
     def __init__(self, baseline_name='glomap', baseline_folder='glomap'):
 
-        default_parameters = {'verbose': 1, 'matcher_type': 'exhaustive', 'use_gpu': 1, 'max_rgb': 200}
+        default_parameters = {'verbose': 1, 'mode': 'mono', 
+                              'matcher_type': 'exhaustive', 'use_gpu': 1, 'max_rgb': 200}
 
         # Initialize the baseline
         super().__init__(baseline_name, baseline_folder, default_parameters)
+        self.color = 'green'
+        self.modes = ['mono']
 
     def build_execute_command(self, exp_it, exp, dataset, sequence_name):
         return super().build_execute_command_cpp(exp_it, exp, dataset, sequence_name)
 
-    def is_cloned(self):
-        return os.path.isdir(os.path.join(self.baseline_path, '.git'))
-    
     def git_clone(self):
         colmap = COLMAP_baseline()
         if not colmap.is_cloned():
@@ -28,8 +27,4 @@ class GLOMAP_baseline(BaselineVSLAMLab):
         super().git_clone()
 
     def is_installed(self): 
-        return True
-
-    def info_print(self):
-        super().info_print()
-        print(f"Default executable: {os.path.join(self.baseline_path, 'glomap_reconstruction.sh')}")
+        return (True, 'is installed') if self.is_cloned() else (False, 'not installed (conda package available)')
